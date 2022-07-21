a381836dcff2ce4966e54632767a9c660bb1a88e
S.Ghanesh Karthick <ghanesh.karthick@S.Ghanesh-Karthick's-MacBook---RFXYLWVYKN>
2022-07-21 13:46:05 +0530

    Adding Route Tables for nat and IGW

---
+++ Routes.tf
@@ -0,0 +1,45 @@
+resource "aws_route_table" "Int_route_tbl" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Internet_Gateway.id
+  }
+
+
+
+  tags = {
+    Name = "Internet_Route_Table"
+  }
+}
+
+resource "aws_route_table" "nat_route_tbl" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.NAT_Gw.id
+  }
+
+
+
+  tags = {
+    Name = "nat_Route_Table"
+  }
+}
+
+resource "aws_route_table_association" "Public_Subnet_IGW" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.Int_route_tbl.id
+}
+
+resource "aws_route_table_association" "Private_Subnet_Nat" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.nat_route_tbl.id
+}
+
+
+resource "aws_route_table_association" "data" {
+  subnet_id      = aws_subnet.data.id
+  route_table_id = aws_route_table.nat_route_tbl.id
+}
